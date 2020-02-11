66a5dfbb81dac3afba3dc436b79f38969c27e2fa
us869 <42154943+us869@users.noreply.github.com>
2020-02-11 22:07:47 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers/root-reducer';
+import { Provider } from 'react-redux';
+import { composeWithDevTools} from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+class App extends Component {
+
+    render() {
+        return (
+            <Provider store={store}>
+                <div className="container">
+                    <h1 className="alert alert-info">React-Redux Demo App</h1>
+                    <div className="row">
+                        <div className="col">
+                            <ContactForm />
+                        </div>
+                        <div className="col">
+                            <ContactList />
+                        </div>
+                    </div>
+                </div>
+            </Provider>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
